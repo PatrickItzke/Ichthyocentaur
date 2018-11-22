033e784349cc7ec71df065ba1421034d3db816dc
Patrick Itzke <Pat@Patricks-MBP-2.fritz.box>
2018-11-22 21:32:02 +0100

    Added topic dispatcher

---
+++ geoanalysis/analysis_dispatching/analysis_dispatcher.py
@@ -0,0 +1,34 @@
+
+import json
+import time
+import pika
+
+
+CONNECTION = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+CHANNEL = CONNECTION.channel()
+
+CHANNEL.queue_declare(queue='analysis_task_queue', durable=False)
+
+queues = {
+    'precipitation': 'precipiation_lookup_task_queue'
+}
+
+#Iterate over the queues dict and initialize queues
+for key, value in queues.items():
+    CHANNEL.queue_declare(queue=value, durable=False)
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    body = json.loads(body)
+    
+    for topic in body['topics']:
+       CHANNEL.basic_publish('', queues[topic], json.dumps(body))
+
+    print(" [x] Topic dispatching: %r" % json.dumps(body))   
+    print(" [x] Done")
+
+CHANNEL.basic_consume(callback, queue='analysis_task_queue')
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+CHANNEL.start_consuming()
